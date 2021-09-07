89b78d8520b8f21f9afb3bab48632f52a9b40417
luckyramisap <89761887+luckyramisap@users.noreply.github.com>
2021-09-07 17:10:55 +0700

    Create sublime.js

---
+++ sublime.js
@@ -0,0 +1,6 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js' type='text/javascript'/>
+<script type='text/javascript'>
+//<![CDATA[
+hljs.initHighlightingOnLoad();hljs.configure({useBR:false});$("div.code").each(function(e,t){hljs.highlightBlock(t)});
+//]]>
+</script>
